5a822394978f6ef347526e448b37e89a3dbbd3b6
aidan <aidan@xmlteam.com>
2023-04-28 13:53:46 -0400

    apr 28

--- commentcheck.py
+++ commentcheck.py
@@ -18,7 +18,7 @@ reddit = praw.Reddit(
     user_agent="television fetch by u/Grubster11",
 )
 
-submission = reddit.submission("12eo2wc")
+submission = reddit.submission("12u5o5a")
 submission.comments.replace_more(limit=None)
 
 with open(f"{date}-allShows.json", 'r') as json_file:
