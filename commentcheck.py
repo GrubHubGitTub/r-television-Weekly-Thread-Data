5fcdba9d8336ec8c07237bf1e9610b0fb12c5c22
GrubHubGitTub <aidangruber@hotmail.com>
2023-05-27 12:51:08 +0700

    week 21

--- commentcheck.py
+++ commentcheck.py
@@ -18,7 +18,7 @@ reddit = praw.Reddit(
     user_agent="television fetch by u/Grubster11",
 )
 
-submission = reddit.submission("138ra4k")
+submission = reddit.submission("13lxzun")
 submission.comments.replace_more(limit=None)
 
 with open(f"{date}-allShows.json", 'r') as json_file:
